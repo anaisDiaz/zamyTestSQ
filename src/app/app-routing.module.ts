b939a576395a437cd11b0fcbdff7e0dec8950cb2
kath2377 <kjav2377@gmail.com>
2018-06-03 20:35:10 -0500

    firebase authentication configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { EventRegistrationComponent } from './events/event-registration/event-registration.component';
 import { EventListComponent } from './events/event-list/event-list.component';
 import { EventDetailComponent } from './events/event-detail/event-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'event-registration', component: EventRegistrationComponent },
   { path: 'event/all', component: EventListComponent },
-  { path: 'event/:id', component: EventDetailComponent}
+  { path: 'event/:id', component: EventDetailComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventRegistrationComponent } from './events/event-registration/event-re
 import { EventListComponent } from './events/event-list/event-list.component';
 import { EventDetailComponent } from './events/event-detail/event-detail.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -13,6 +14,9 @@ import { FirebaseService } from './services/firebase.service';
 import { UserService } from './services/user.service';
 import { EventService } from './services/event.service';
 import { AttendanceListComponent } from './attendance/attendance-list/attendance-list.component';
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './login/login.component';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCOu0Lwpp5d_eRnIyJHcVypT2E-UD9HF8A',
@@ -29,15 +33,19 @@ const firebaseConfig = {
     EventRegistrationComponent,
     EventListComponent,
     EventDetailComponent,
-    AttendanceListComponent
+    AttendanceListComponent,
+    LoginComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule,
     AppRoutingModule
   ],
   providers: [
+    AuthService,
+    AngularFireAuth,
     FirebaseService,
     UserService,
     EventService],
