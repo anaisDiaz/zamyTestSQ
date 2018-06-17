f42cb6371548ada45a54ee6606ffe2c318bf3a6e
kath2377 <kjav2377@gmail.com>
2018-06-16 19:17:24 -0500

    added array of users

--- src/app/attendance/attendance-list/attendance-list.component.ts
+++ src/app/attendance/attendance-list/attendance-list.component.ts
@@ -5,6 +5,7 @@ import { User } from '../../models/user.model';
 import { AttendanceService } from '../../services/attendance.service';
 import { Participant } from '../../models/participant.model';
 import { EventService } from '../../services/event.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-attendance-list',
@@ -18,15 +19,24 @@ export class AttendanceListComponent implements OnInit, OnDestroy {
   routeSubscriber: any;
   participantsSubscriber: any;
   eventSubscriber: any;
+  users: User[];
+  user: User;
 
   constructor(private attendanceService: AttendanceService, private activatedRoute: ActivatedRoute,
-    private eventService: EventService) { }
+    private eventService: EventService, private userService: UserService) { }
 
   ngOnInit() {
     this.routeSubscriber = this.activatedRoute.params.subscribe(params => this.eventId = params['id']);
     this.eventSubscriber = this.eventService.getEventById(this.eventId).subscribe(event => this.event = event);
     this.participantsSubscriber = this.attendanceService.getParticipants(this.eventId)
-      .subscribe(participants => this.participants = participants);
+      .subscribe(participants => {
+        this.participants = participants;
+        this.participants.forEach(participant => {
+          this.userService.getUserByUsername(participant.id).subscribe(user => this.user = user);
+          this.users.push(this.user);
+        }
+        );
+      });
   }
 
   ngOnDestroy() {
