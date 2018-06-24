75f1542286efe363cd3c69b386438f4a66da6939
anaisDiaz <annie.08.12.94@gmail.com>
2018-06-24 09:58:00 -0500

    update component my event list

--- src/app/components/users/my-event-list/my-event-list.component.ts
+++ src/app/components/users/my-event-list/my-event-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Event } from '../../../models/event.model';
+import { AuthService } from '../../../services/firebase-services/auth.service';
+import { AppSettings } from '../../../app.settings';
 
 @Component({
   selector: 'app-my-event-list',
@@ -7,7 +9,7 @@ import { Event } from '../../../models/event.model';
   styleUrls: ['./my-event-list.component.css']
 })
 export class MyEventListComponent implements OnInit {
-
+  isAdmin: Boolean;
   myEvents = [new Event('EventA1', 'asas', 'asasa', new Date(), 0,
     new Date(), 'http://s2.subirimagenes.com/otros/previo/thump_9855489il570xn.jpg'),
   new Event('EventA2', 'asasass', 'asasasasa', new Date(), 0, new Date(),
@@ -16,9 +18,12 @@ export class MyEventListComponent implements OnInit {
     'http://s2.subirimagenes.com/otros/previo/thump_9855489il570xn.jpg'), new Event('EventA4', 'asasass', 'asasasasa',
       new Date(), 0, new Date(), 'http://s2.subirimagenes.com/otros/previo/thump_9855489il570xn.jpg')];
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getAuth().forEach(authUser => {
+      this.isAdmin = authUser.email.replace(AppSettings.emailDomain, '').endsWith(AppSettings.adminSuffix);
+    });
   }
 
 }
