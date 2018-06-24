ee514e92468a59e2eac79ecbc60d78f6de14a947
kath2377 <kjav2377@gmail.com>
2018-06-23 19:53:44 -0500

    index modified

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,9 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
-// Configure the email transport using the default SMTP transport and a GMail account.
-// For Gmail, enable these:
+// Habilitar en gmail:
 // 1. https://www.google.com/settings/security/lesssecureapps
 // 2. https://accounts.google.com/DisplayUnlockCaptcha
-// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
-// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 const mailTransport = nodemailer.createTransport(smtpTransport({
