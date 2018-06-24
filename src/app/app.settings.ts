97f5e9fabe5803bd2fe318cf327df5ef55ff5517
kath2377 <kjav2377@gmail.com>
2018-06-24 16:53:08 -0500

    format

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -11,7 +11,7 @@ export class AppSettings {
         '<p> Accede al siguiente link: {{mainURL}} </p> <p>Gracias!'.replace('{{mainURL}} </p>', AppSettings.mainURL);
     public static rejectionMailSubject = 'Solicitud no aprobada - Zamy Club';
     public static rejectionMailBody = '<img src="https://firebasestorage.googleapis.com' +
-    '/v0/b/zamy-d9d98.appspot.com/o/zamy-web%2FlogoZamy2.jpg?alt=media&token=b4a19afa-' +
-    '022b-4359-8e9a-862cae709727" alt="Avatar" style="width:15%">' +
-    '<p> Hola {{firstName}}, lo sentimos.</p><p>Tu solicitud fue denegada por la siguiente razon: </p>';
+        '/v0/b/zamy-d9d98.appspot.com/o/zamy-web%2FlogoZamy2.jpg?alt=media&token=b4a19afa-' +
+        '022b-4359-8e9a-862cae709727" alt="Avatar" style="width:15%">' +
+        '<p> Hola {{firstName}}, lo sentimos.</p><p>Tu solicitud fue denegada por la siguiente razon: </p>';
 }
