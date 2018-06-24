ae2b20c576a6a624dc023fe63d31725854dad81d
anaisDiaz <annie.08.12.94@gmail.com>
2018-06-24 09:37:19 -0500

    modified ts

--- src/app/components/users/my-event-list/my-event-list.component.ts
+++ src/app/components/users/my-event-list/my-event-list.component.ts
@@ -7,16 +7,9 @@ import { Event } from '../../../models/event.model';
   styleUrls: ['./my-event-list.component.css']
 })
 export class MyEventListComponent implements OnInit {
-  //myEvent: Event[];
   constructor() { }
 
   ngOnInit() {
   }
-
-  myEvents=[
-    new Event('EventA1', 'asas', 'asasa', new Date(), 0, new Date(), 'http://s2.subirimagenes.com/otros/previo/thump_9855489il570xn.jpg'), 
-    new Event('EventA2', 'asasass', 'asasasasa', new Date(), 0, new Date(), 'http://s2.subirimagenes.com/otros/previo/thump_9855489il570xn.jpg'), 
-    new Event('EventA3', 'asas', 'asasa', new Date(), 0, new Date(), 'http://s2.subirimagenes.com/otros/previo/thump_9855489il570xn.jpg'), 
-    new Event('EventA4', 'asasass', 'asasasasa', new Date(), 0, new Date(), 'http://s2.subirimagenes.com/otros/previo/thump_9855489il570xn.jpg')];
 }
 
