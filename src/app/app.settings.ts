8f8892f92f54dfe7548403c9586ae7fd6205ed3d
kath2377 <kjav2377@gmail.com>
2018-06-24 16:52:44 -0500

    added image to body of email

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -4,8 +4,14 @@ export class AppSettings {
     public static imageFileExtension = '.jpg';
     public static mainURL = 'http://localhost:4200';
     public static welcomeMailSubject = 'Bienvenido(a) a Zamy Club!!!';
-    public static welcomeMailBody = 'Hola {{firstName}} !! <br> Te damos la bienvenida a zamy.' +
-    '<br> Accede al siguiente link: {{mainURL}} <br> Gracias!'.replace('{{mainURL}}', AppSettings.mainURL);
+    public static welcomeMailBody = '<img src="https://firebasestorage.googleapis.com' +
+        '/v0/b/zamy-d9d98.appspot.com/o/zamy-web%2FlogoZamy2.jpg?alt=media&token=b4a19afa-' +
+        '022b-4359-8e9a-862cae709727" alt="Avatar" style="width:15%">' +
+        '<p> Hola {{firstName}} !! </p><p> Te damos la bienvenida a zamy.</p>' +
+        '<p> Accede al siguiente link: {{mainURL}} </p> <p>Gracias!'.replace('{{mainURL}} </p>', AppSettings.mainURL);
     public static rejectionMailSubject = 'Solicitud no aprobada - Zamy Club';
-    public static rejectionMailBody = 'Hola {{firstName}}, lo sentimos. <br> Tu solicitud fue denegada por la siguiente razon: <br>';
+    public static rejectionMailBody = '<img src="https://firebasestorage.googleapis.com' +
+    '/v0/b/zamy-d9d98.appspot.com/o/zamy-web%2FlogoZamy2.jpg?alt=media&token=b4a19afa-' +
+    '022b-4359-8e9a-862cae709727" alt="Avatar" style="width:15%">' +
+    '<p> Hola {{firstName}}, lo sentimos.</p><p>Tu solicitud fue denegada por la siguiente razon: </p>';
 }
