81d57c13001c7fa51102f3a92ba149860a1f2e0b
kath2377 <kjav2377@gmail.com>
2018-06-25 02:18:26 -0500

    finished

--- src/app/components/users/password-restoration/password-restoration.component.ts
+++ src/app/components/users/password-restoration/password-restoration.component.ts
@@ -1,37 +1,30 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../../services/firebase-services/auth.service';
-import { UserService } from '../../../services/user-service/user.service';
+import { AppSettings } from '../../../app.settings';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-password-restoration',
   templateUrl: './password-restoration.component.html',
   styleUrls: ['./password-restoration.component.css']
 })
-export class PasswordRestorationComponent implements OnInit, OnDestroy {
+export class PasswordRestorationComponent implements OnInit {
   username: string;
-  subscriber: any;
-  message: string;
-  warningMessage: string;
 
-  constructor(private authService: AuthService, private userService: UserService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   sendRestorationEmail(): void {
     if (this.username !== undefined) {
-      this.message = 'Recibiras un correo para la restauracion de tu contraseña.';
-      this.subscriber = this.userService.getUserByUsername(this.username).subscribe(user => {
-        this.authService.resetPassword(user.email).catch(err => console.log('error: ' + err));
-      });
+      this.authService.resetPassword(this.username + AppSettings.emailDomain).catch(err => console.log('error: ' + err));
     } else {
-      this.warningMessage = 'Ingrese un username';
       console.log('no username');
     }
   }
 
-  ngOnDestroy() {
-    this.subscriber.unsubscribe();
+  goToLogin() {
+    this.router.navigate(['login']);
   }
-
 }
