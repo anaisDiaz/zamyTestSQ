b90ed8d5647058d109b1c00435f113b02091c098
Kathlen Jazmin Apaza Vera <kapazave@everis.com>
2018-06-25 13:37:13 -0500

    fixed

--- src/app/components/events/event-editing/event-editing.component.ts
+++ src/app/components/events/event-editing/event-editing.component.ts
@@ -30,7 +30,9 @@ export class EventEditingComponent implements OnInit {
 
   ngOnInit() {
     this.routeSubscriber = this.activatedRoute.params.subscribe(params => this.eventId = params['id']);
-    this.eventSubscriber = this.eventService.getEventById(this.eventId).subscribe(event => this.event = event);
+    this.eventSubscriber = this.eventService.getEventById(this.eventId).subscribe(event =>{
+      this.event = event;
+    });
   }
 
   uploadImage(uploadEvent) {
