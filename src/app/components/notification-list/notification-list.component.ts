baf418cf48c027b1c04b0ea565b680130b256b3c
kath2377 <kjav2377@gmail.com>
2018-06-26 07:31:56 -0500

    done

--- src/app/components/notification-list/notification-list.component.ts
+++ src/app/components/notification-list/notification-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Notification } from '../../models/notification.model';
 import { NotificationService } from '../../services/notification-service/notification.service';
 import { AuthService } from '../../services/firebase-services/auth.service';
@@ -15,7 +15,6 @@ export class NotificationListComponent implements OnInit, OnDestroy {
   notifications: Notification[] = [];
   participantId: string;
   subscriber: any;
-  alert: boolean;
 
   constructor(private notificationService: NotificationService, private authService: AuthService,
     private attendanceService: AttendanceService, private router: Router) { }
@@ -27,9 +26,7 @@ export class NotificationListComponent implements OnInit, OnDestroy {
         notifications.forEach(notification => {
           this.attendanceService.getAttendanceById(notification.eventId, this.participantId).subscribe(
             participant => {
-              this.alert = true;
               if (participant.inscriptionDate !== undefined && !this.notifications.find(n => n.eventId === notification.eventId)) {
-                
                 this.notifications.push(notification);
               }
             }
@@ -41,15 +38,6 @@ export class NotificationListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscriber.unsubscribe();
-  }
-
-  inactiveAlert() {
-    this.alert = false;
-  }
-
-  goToEventDetails(eventId: string): void {
-    console.log('eventId = ' + eventId);
-    this.router.navigate(['event', eventId]);
   }
 
 }
